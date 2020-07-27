6a9f17c5e3bc41fa5fdc0299ba2b57a6068a99be
Romeo Šajina <romeo.sajina@gmail.com>
2020-07-27 02:21:48 +0200

    test dataset iterator and debug method

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 import cv2
 from exception import SignatureException
@@ -14,6 +15,26 @@ gotse = GlobalOtsuTresholdSignatureExtractor()
 ttse = TriangleTresholdSignatureExtractor()
 ytse = YenTresholdSignatureExtractor()
 maskse = MaskedSignatureExtractor()
+
+
+class Signatures:
+
+    def __init__(self, dataset_folder="./images/original/"):
+        self.images = os.listdir(dataset_folder)
+        self.images.sort()
+        self.n = 0
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < len(self.images):
+            result = self.images[self.n]
+            self.n += 1
+            return result
+        else:
+            raise StopIteration
 
 
 def plot_sig(_se, _ax, _img, _title):
@@ -38,19 +59,15 @@ def plot_sig(_se, _ax, _img, _title):
         _ax.text(0, 25, e.error_code, color="red") # e.message
 
 
-def run_for_dataset(dataset_index=0):
+def run_for_all():
 
-    datasets = [[15, "jpeg"], [35, "jpg"], [15, "png"]]
+    for i, fn in enumerate(Signatures()):
 
-    size, ext = datasets[dataset_index]
-
-    for i in range(size+1):
-
-        img = cv2.imread("./images/original/eg{0}.{1}".format(i, ext), cv2.IMREAD_UNCHANGED)
+        img = cv2.imread("./images/original/{0}".format(fn), cv2.IMREAD_UNCHANGED)
 
         assert img is not None, "Image cannot be none!"
 
-        cv2.imwrite("./images/out/eg_{0}_{1}_0.png".format(dataset_index, i), img)
+        cv2.imwrite("./images/out/{0}_0.png".format(i), img)
 
         fig, ((ax0, ax1), (ax2, ax3), (ax4, ax5), (ax6, ax7), (ax8, ax9)) = plt.subplots(5, 2)
 
@@ -72,14 +89,8 @@ def run_for_dataset(dataset_index=0):
             ax.set_yticks(())
             ax.set_xticks(())
 
-        plt.savefig("./images/out/eg_{0}_{1}_1.png".format(dataset_index, i))
+        plt.savefig("./images/out/{0}_1.png".format(i))
         plt.close(plt.gcf())
-
-
-def run_all():
-    run_for_dataset(0)
-    run_for_dataset(1)
-    run_for_dataset(2)
 
 
 def run_for_file(src="./images/nt.png"):
@@ -89,13 +100,49 @@ def run_for_file(src="./images/nt.png"):
 
     img = cv2.imread(src, cv2.IMREAD_UNCHANGED)
 
+    agtse.pre_validate(img)
+
     sig = agtse.extract_and_resize(img, size=(500, 50))
     sig = agtse.prettify(sig)
 
     cv2.imwrite(src.replace("."+ext, "_1.png"), sig)
 
 
+def debug(se_class, dataset_folder="./images/original/"):
+
+    verbose_folder = "./__verbose__/final/"
+    os.makedirs(verbose_folder, exist_ok=True)
+
+    for i, fn in enumerate(Signatures(dataset_folder)):
+        se = se_class(verbose=2, img_name=str(i))
+
+        src = dataset_folder + fn
+        img = cv2.imread(src, cv2.IMREAD_UNCHANGED)
+
+        try:
+            se.pre_validate(img)
+        except SignatureException as e:
+            print(i, e.error_code)
+
+        sig = se.extract_and_resize(img, size=(500, 50))
+        sig = se.prettify(sig)
+
+        try:
+            se.validate(sig)
+        except SignatureException as e:
+            print(i, e.error_code)
+
+        se._verbose(sig, "final")
+        cv2.imwrite(verbose_folder + str(i) + "_0.png", img)
+        cv2.imwrite(verbose_folder + str(i) + "_1.png", sig)
+
+
 if __name__ == "__main__":
 
-    run_all()
+    #run_for_all()
+    #debug(AdaptiveGaussianTresholdSignatureExtractor)
+    import time
+    start_time = time.time()
+    debug(AdaptiveGaussianTresholdSignatureExtractor)
+    print("--- %s seconds ---" % (time.time() - start_time))
     pass
